bcc806750709ce1c6e91b1bb1ca40ce66c6b086c
PARTHIBAN-007 <157013976+PARTHIBAN-007@users.noreply.github.com>
2024-12-05 18:20:51 +0530

    Logistic Regression

---
+++ 4 Logistic Regression.py
@@ -0,0 +1,37 @@
+import numpy as np
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, iterations=1000):
+        self.lr = learning_rate
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.iterations):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_pred = self.sigmoid(linear_model)
+
+            dw = (1 / n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / n_samples) * np.sum(y_pred - y)
+
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict_proba(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        return self.sigmoid(linear_model)
+
+    def predict(self, X, threshold=0.5):
+        y_pred_proba = self.predict_proba(X)
+        return (y_pred_proba >= threshold).astype(int)
+    
